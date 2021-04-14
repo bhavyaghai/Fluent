548f9143e50379bb376526ea8e6099b8feab72b7
Bhavya Ghai <bhavyaghai@gmail.com>
2021-04-14 15:31:12 -0400

    new results simulation

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ function wrap_span(res) {
     if (bias_score >= thresh) {
       span = jQuery("<span></span>").attr({
         tabindex: "0",
-        title: "<b>" + word + ":</b>" + bias_score,
+        //title: "<b>" + word + ":</b>" + bias_score*100+"%",
         "data-trigger": "hover",
         "data-placement": "bottom",
         "data-toggle": "popover",
